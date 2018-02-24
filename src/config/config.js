d7adfca1922199884aa2de5412aadfa5cc70f485
Antoine Gaillot <manzagregine@gmail.com>
2018-02-24 12:59:53 +0100

    Adding dotenv for .env files

---
+++ src/config/config.js
@@ -0,0 +1,15 @@
+import path from 'path'
+
+const env = process.env.NODE_ENV || 'development'
+
+require('dotenv-safe').load({
+    path: path.join(__dirname, `../.env.${env}`),
+    sample: path.join(__dirname, '../.env.example'),
+})
+
+module.exports = {
+    env,
+    logs: env === 'production' ? 'combined' : 'dev',
+    port: process.env.PORT || 3000,
+    enable: process.env.ENABLE_API || false,
+}
