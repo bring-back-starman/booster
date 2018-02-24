fb8965541f4787cd6e796db438454cc24a4b6dff
Antoine Gaillot <manzagregine@gmail.com>
2018-02-24 13:26:49 +0100

    Updating config

--- src/index.js
+++ src/index.js
@@ -1,40 +1,41 @@
-import http from 'http'
-import express from 'express'
-import cors from 'cors'
-import morgan from 'morgan'
-import bodyParser from 'body-parser'
+import bodyParser from 'body-parser';
+import compression from 'compression';
+import cors from 'cors';
+import express from 'express';
+import http from 'http';
+import morgan from 'morgan';
 
-import initializeDb from './db'
-import middleware from './middleware'
-import api from './api'
-import config from './config.json'
+import api from './api';
+import config from './config/config';
+import initializeDb from './db';
+import middleware from './middleware';
 
-const app = express()
-app.server = http.createServer(app)
+const app = express();
+app.server = http.createServer(app);
 
 // logger
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 // 3rd party middleware
-app.use(cors({
-    exposedHeaders: config.corsHeaders,
-}))
+app.use(cors());
 
 app.use(bodyParser.json({
-    limit: config.bodyLimit,
-}))
+  limit: config.bodyLimit,
+}));
+
+app.use(compression());
 
 // connect to db
-initializeDb(db => {
-    // internal middleware
-    app.use(middleware({ config, db }))
-    
-    // api router
-    app.use('/api', api({ config, db }))
-    
-    app.server.listen(process.env.PORT || config.port, () => {
-        console.log(`Started on port ${app.server.address().port}`)
-    })
-})
+initializeDb((db) => {
+  // internal middleware
+  app.use(middleware({ config, db }));
 
-export default app
+  // api router
+  app.use('/api', api({ config, db }));
+
+  app.server.listen(config.port, () => {
+    console.log(`Started on port ${config.port} (${config.env})`);
+  });
+});
+
+export default app;
